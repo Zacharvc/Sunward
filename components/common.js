43046de08e749fe1e73e06b8a5709ecb412251ea
YooLan <61037787+Zacharvc@users.noreply.github.com>
2023-08-19 22:41:18 +0800

    upload common.js

--- components/common.js
+++ components/common.js
@@ -1,1 +1,38 @@
+"use strict";
 
+// 转发消息制作
+async function generateForwardMsg (e, title = false, forwardMsg = []) {
+	// 频道则返回
+	if (e.QQGuild) return forwardMsg;
+	
+	if (!Array.isArray(forwardMsg)) forwardMsg = [forwardMsg];
+	
+	if (e.isGroup) forwardMsg = await e.group.makeForwardMsg(forwardMsg);
+	else if (e.isPrivate) forwardMsg = await e.friend.makeForwardMsg(forwardMsg);
+	else return forwardMsg;
+	
+	if (title) {
+		forwardMsg.data = forwardMsg.data
+		.replace(/\n/g, "")
+		.replace(/<title color="#777777" size="26">(.+?)<\/title>/g, "___")
+		.replace(/(___)+/g, `<title color="#777777" size="26">${title}</title>`);
+	}
+	
+	return forwardMsg;
+};
+
+// 字符串中部加密
+async function codeString (rawStr, replaceTo = "*") {
+	if (!rawStr) return false;
+	rawStr = String(rawStr);
+	if (rawStr.length <= 2) return replaceTo + replaceTo;
+	let startAim = rawStr.length / 3, endAim = startAim * 2;
+	let replaceStr = function (length) {
+		let tempStr = "";
+		for (let i = 0; i < Number(length); i++) tempStr += replaceTo;
+		return tempStr;
+	}(Math.round(endAim) - Math.round(startAim));
+	return rawStr.slice(0, Math.round(startAim)) + replaceStr + rawStr.slice(Math.round(endAim));
+};
+
+export default { generateForwardMsg, codeString };
