034d5eed47d273dc49615d1a52ff3cda29f79b29
YooLan <61037787+Zacharvc@users.noreply.github.com>
2023-08-20 08:25:44 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,30 @@
 
 // 导入包
 import fs from "node:fs";
+import YAML from "yaml";
 
-// 插件名称
+// 插件目录名称
 const plugin = "Sunward";
-
-// 载入欢迎语
-const welcomeTips = "感谢使用向阳插件，您的支持是我更新的动力！";
 
 // 获取插件目录文件
 const appFiles = fs.readdirSync(`./plugins/${plugin}/apps`).filter( (file) => file.endsWith(".js") );
+// 读取package.yaml
+const $Package = YAML.parse(fs.readFileSync(`./plugins/${plugin}/package.yaml`, "utf-8"));
+// 插件名称
+const pluginName = $Package.name;
+const pluginNameCn = $Package.cNname;
+// 插件版本
+const pluginVersion = $Package.version;
+// 插件作者
+const pluginAuthor = $Package.author;
+
+// 插件初始化欢迎语
+let welcomeTips = [
+	`------------------------`,
+	`欢迎使用${pluginNameCn}(${pluginName})插件`,
+	`当前版本：${pluginVersion}`,
+	`您的使用就是对我莫大的鼓励！`
+];
 
 async function allInitial (Client) {
 	// 没有机器人全局变量则跳出
@@ -36,9 +51,9 @@ async function allInitial (Client) {
 		}
 		return apps;
 	}();
-	logger.info("---------------------");
-	logger.info(welcomeTips);
-	// 返回apps
+	// 输出欢迎语
+	welcomeTips.forEach( (tip) => { logger.info(tip) });
+	// 输出apps
 	return apps;
 };
 
