c992b54bc00fdc5cc91618f5815ec3974885b6c4
YooLan <61037787+Zacharvc@users.noreply.github.com>
2023-08-21 12:32:46 +0800

    更新 package.js

--- components/package.js
+++ components/package.js
@@ -2,7 +2,7 @@
 
 import fs from "node:fs";
 import YAML from "yaml";
-import pluginRootDir from "./path.js";
+import { pluginRootDir } from "./path.js";
 
 // 读取package.yaml
 const $Package = YAML.parse(fs.readFileSync(`${pluginRootDir}/package.yaml`, "utf-8"));
