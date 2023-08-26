d39d2c05e633189b4bff8430c56edcffececa9b8
YooLan <61037787+Zacharvc@users.noreply.github.com>
2023-08-26 19:57:39 +0800

    修改#退出群聊

--- apps/quitGroup.js
+++ apps/quitGroup.js
@@ -25,29 +25,27 @@ export class quitGroup extends plugin {
 			e.reply("请发送【#退出群聊 + 对应群号或者对应码】", true);
 			return;
 		}
-		// 重新载入群聊
-		await e.bot.reloadGroupList();
 		// 遍历目标
-		let quitNum = 0;
 		await groups.forEach( async (group) => {
 			let replyMsg = await this.quitTargetGroup(e, group);
 			await e.reply(replyMsg, false);
-			quitNum++;
 		});
 		// 多群提示
-		if (quitNum > 1) await e.reply("退出群聊命令执行完毕", true);
+		if (groups.length > 1) await e.reply("退出群聊命令执行完毕", true);
 	};
 	
 	async quitTargetGroup (event, targetGroup) {
 		let e = event;
 		// 目标群聊
 		let target = targetGroup;
+		// 重新载入群聊
+		await e.bot.reloadGroupList();
 		// 加载群列表
 		let groups = Object.keys(e.bot.gl);
 		// 是否存在群聊
 		if (String(targetGroup).startsWith("G")) {
 			// 获取Redis
-			let group2code = JSON.parse(redis.get("Sunward:groups-code"));
+			let group2code = JSON.parse(await redis.get("Sunward:groups-code"));
 			// 查找群聊
 			if (!Object.keys(group2code).includes(targetGroup)) return `没有找到对应群聊：${targetGroup}`;
 			// 确定目标
@@ -58,7 +56,7 @@ export class quitGroup extends plugin {
 		// 是否为当前群聊
 		if (e.isGroup && e?.group_id == target) return "请在私聊或其他群里中使用";
 		// 退出群聊
-		e.bot.pickGroup(target).quit();
+		await e.bot.pickGroup(target).quit();
 		return `已退出群聊：${target}`;
 	};
 	
