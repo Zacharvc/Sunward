2285716e7c502f169b29682d1980fc704b319381
WithGarlic <61037787+Zacharvc@users.noreply.github.com>
2023-09-29 00:04:58 +0800

    修改 #安装插件 功能
    Signed-off-by: WithGarlic <61037787+Zacharvc@users.noreply.github.com>

--- apps/installPlugin.js
+++ apps/installPlugin.js
@@ -33,30 +33,21 @@ export class installPlugin extends plugin {
 			this.reply("数据不完整，请重新输入！");
 			return;
 		}
-		let command = `git clone ${url}.git ./plugins/${pluginName}/`;
+		let command = `git clone ${url}.git ./plugins/${pluginName}`;
 		// 提示
 		this.reply("开始安装插件：" + pluginName);
 		// 执行操作
 		exec(command, { cwd: $path }, async (error, stdout, stderr) => {
-			// 插件已存在
-			if (/(already exists and is not an empty directory)/.test(stdout)) {
-				this.reply(`${pluginName} 已存在，安装已停止`);
-				return true;
-			}
 			// 安装出错
-			if (/(does not exist)/.test(stdout)) {
-				this.reply("项目地址不存在，请重新输入！");
-				return true;
-			}
 			if (error) {
 				logger.error(`Error code：${error.code}`);
-				logger.error(`Error trick：${error.trick}`);
+				logger.error(stderr);
 				this.reply(`${pluginName} 安装出错，请稍后再试！`);
 				setTimeout( () => { this.reply("错误信息已输出到日志") }, 500);
 				return true;
 			}
 			// 安装成功
-			this.reply(`${pluginName} 安装成功！`);
+			this.reply(`${pluginName} 项目克隆成功，请自行重启以应用插件`);
 			return true;
 		});
 	};
