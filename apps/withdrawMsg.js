d81431682bbb4c6b67cf3aa8fa46795f62c7a9ae
WithGarlic <61037787+Zacharvc@users.noreply.github.com>
2023-10-01 16:54:55 +0800

    新增 #投票撤回
    Signed-off-by: WithGarlic <61037787+Zacharvc@users.noreply.github.com>

--- apps/withdrawMsg.js
+++ apps/withdrawMsg.js
@@ -11,6 +11,9 @@ export class withdrawMsg extends plugin {
 			rule: [{
 				reg: "^#*撤回(消息)?$",
 				fnc: "withdrawMessage"
+			}, {
+				reg: "^#*(公投|票选|投票)撤回(消息)?$",
+				fnc: "voteToWithdrawMsg"
 			}]
 		});
 	};
@@ -45,7 +48,38 @@ export class withdrawMsg extends plugin {
 				// 如果是撤回自己
 				let me = e.group.pickMember(e.user_id);
 				if (e.source.user_id === e.user_id || e.isMaster || me.is_owner || me.is_admin) await e.group.recallMsg(e.source.seq);
-				else this.reply("很抱歉，您无权这样做");
+				else this.reply("很抱歉，您无权这样做，或者您可以使用指令：#投票撤回");
+			}
+		}
+	};
+	
+	async voteToWithdrawMsg () {
+		let e = this.e;
+		// 仅群聊
+		if (!e.isGroup) return;
+		// 是否引用消息
+		if (!e.hasReply && !e.source) return;
+		// 撤回自己
+		if (e.source.user_id === e.self_id) await this.withdrawMessage();
+		// 有无权限
+		else if (!this.hasPower(e, e.source.user_id)) this.reply("发起失败，我无权撤回他人消息");
+		else {
+			let needNum = 2;
+			// 获取Redis
+			let voteNum = await redis.get(`Sunward:voteToWithdrawMsg:${e.group_id}:${e.source.seq}`);
+			// 是否首次投票
+			if (!voteNum) voteNum = 0;
+			// 判断人数是否超过指定
+			if (voteNum >= needNum) {
+				await e.group.recallMsg(e.source.seq);
+				await redis.del(`Sunward:voteToWithdrawMsg:${e.group_id}:${e.source.seq}`);
+				this.reply("投票撤回成功，已撤回目标消息");
+			} else {
+				voteNum++;
+				if (voteNum === 1) await this.reply(`${e.member.card} 发起了撤回投票(1/${needNum}), 三分钟后失效`);
+				else if (voteNum > 1) await this.reply(`${e.member.card} 同意了撤回投票(${voteNum}/${needNum})`);
+				// 存储Redis
+				await redis.set(`Sunward:voteToWithdrawMsg:${e.group_id}:${e.source.seq}`, voteNum, { EX: 180 });
 			}
 		}
 	};
