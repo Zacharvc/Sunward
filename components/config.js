21497651516600a0afa1780d560ea7fc8d70df73
WithGarlic <61037787+Zacharvc@users.noreply.github.com>
2023-10-02 11:01:51 +0800

    调
    Signed-off-by: WithGarlic <61037787+Zacharvc@users.noreply.github.com>

--- components/config.js
+++ components/config.js
@@ -8,10 +8,12 @@ async function getKey(fileName, key) {
 	let filePath = `${pluginRootDir}/config/${fileName}.yaml`, defFilePath = `${pluginRootDir}/config/default/${fileName}.yaml`;
 	// 配置文件是否存在
 	if (!fs.existsSync(filePath)) {
-		if (fs.existsSync(defFilePath)) await fs.copyFileSync(defFilePath, filePath);
-		filePath = defFilePath;
+		if (fs.existsSync(defFilePath)) {
+			await fs.copyFileSync(defFilePath, filePath);
+			filePath = defFilePath;
+		} else return null;
 	}
-	// YAM转Json
+	// YAML转Json
 	let configs = await YAML.parse(fs.readFileSync(filePath, "utf-8"));
 	// 是否存在指定键
 	if (key in configs) return configs[key];
